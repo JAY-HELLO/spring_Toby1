33d6ebcc4860d85eb38938aa3657fe4bcf5c34a8
Double_J <double04j@gmail.com>
2023-05-03 17:35:35 +0900

    make tomcat servlet

--- src/main/java/core/spring_toby/SpringTobyApplication.java
+++ src/main/java/core/spring_toby/SpringTobyApplication.java
@@ -2,12 +2,16 @@ package core.spring_toby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class SpringTobyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringTobyApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
