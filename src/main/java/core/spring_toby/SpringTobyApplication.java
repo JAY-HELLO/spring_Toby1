a0396003f22722a9e62a6a2fa281897a345b3246
Double_J <double04j@gmail.com>
2023-05-03 18:06:04 +0900

    front Contoller 기본 구조 제작

--- src/main/java/core/spring_toby/SpringTobyApplication.java
+++ src/main/java/core/spring_toby/SpringTobyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -25,20 +26,32 @@ public class SpringTobyApplication {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         //익명 클래스 코드 작업
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");
+
+                    //인증, 보안, 다국어, 공통기능등을 frontController 에서 처리해야함
+                    if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+                        String name = req.getParameter("name");
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("hello " + name);
+                    }
+                    else if(req.getRequestURI().equals("/user")){
+                        //
+                    }else{
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
                     //웹 응답의 3개 요소
                     //1. 상태라인에서 상태코드
                     //2. 헤더 특히 content type 헤더
                     //3. body 부분
                     // 를 구현해야한다
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("hello " + name);
+
                 }
-            }).addMapping("/hello");// '/hello'로 들어온 리소스는 해당 object가 처리하겠다.
+            }).addMapping("/*");// '/hello'로 들어온 리소스는 해당 object가 처리하겠다. '/*' 경우 모든 리소스를 처리하겠다 (front Controller)
 
         });
         webServer.start();
