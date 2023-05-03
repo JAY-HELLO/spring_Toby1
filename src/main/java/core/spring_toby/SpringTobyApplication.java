b92d9e7f8ead8f9e69978c32283f6d8a870d8bed
Double_J <double04j@gmail.com>
2023-05-03 17:46:51 +0900

    톰캣 컨테이너 띄우고 서블릿 직접 등록해서 띄우는 작업

--- src/main/java/core/spring_toby/SpringTobyApplication.java
+++ src/main/java/core/spring_toby/SpringTobyApplication.java
@@ -4,13 +4,39 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SpringTobyApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        //익명 클래스 코드 작업
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    //웹 응답의 3개 요소
+                    //1. 상태라인에서 상태코드
+                    //2. 헤더 특히 content type 헤더
+                    //3. body 부분
+                    // 를 구현해야한다
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().println("hello servlet");
+
+                    super.service(req, resp);
+                }
+            }).addMapping("/hello");// '/hello'로 들어온 리소스는 해당 object가 처리하겠다.
+
+        });
         webServer.start();
     }
 
