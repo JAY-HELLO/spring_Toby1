0cb6efe52dcaa482b9c26ca0fa05d01ef5779453
Double_J <double04j@gmail.com>
2023-05-03 17:23:20 +0900

    initial struct

---
+++ src/main/java/core/spring_toby/SpringTobyApplication.java
@@ -0,0 +1,13 @@
+package core.spring_toby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTobyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTobyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/core/spring_toby/SpringTobyApplicationTests.java
@@ -0,0 +1,13 @@
+package core.spring_toby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTobyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
