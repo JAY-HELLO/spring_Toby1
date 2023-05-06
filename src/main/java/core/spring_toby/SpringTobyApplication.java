61e6b4c3f85517ed9f710b0d53e689ba7dd628a7
Double_J <double04j@gmail.com>
2023-05-06 19:57:48 +0900

    servlet container 만드는 과정을 스프링 컨테이너가 생성되는 과정에서 생성되도록 변경

--- src/main/java/core/spring_toby/SpringTobyApplication.java
+++ src/main/java/core/spring_toby/SpringTobyApplication.java
@@ -22,21 +22,29 @@ public class SpringTobyApplication {
     public static void main(String[] args) {
 
         //spring 컨테이너 생성
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+            @Override
+            protected void onRefresh() {// onRefresh가 불리는 시점에 초기화 작업 수행
+                super.onRefresh();
+
+                //servlet 컨테이너 생성
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                //익명 클래스 코드 작업
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this) // dispatcherServlet은 웹 어플리케이션 context를 사용해야한다.
+                    ).addMapping("/*");// '/hello'로 들어온 리소스는 해당 object가 처리하겠다. '/*' 경우 모든 리소스를 처리하겠다 (front Controller)
+
+                });
+                webServer.start();
+                
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
-        //servlet 컨테이너 생성
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        //익명 클래스 코드 작업
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext) // dispatcherServlet은 웹 어플리케이션 context를 사용해야한다.
-            ).addMapping("/*");// '/hello'로 들어온 리소스는 해당 object가 처리하겠다. '/*' 경우 모든 리소스를 처리하겠다 (front Controller)
-
-        });
-        webServer.start();
+        
     }
 
 }
