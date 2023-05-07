55983a683c471268e1981693dbde6c76daa9343c
Double_J <double04j@gmail.com>
2023-05-07 20:04:50 +0900

    api를 사용한 test

--- src/test/java/core/spring_toby/HelloApiTest.java
+++ src/test/java/core/spring_toby/HelloApiTest.java
@@ -1,14 +1,11 @@
 package core.spring_toby;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloApiTest {
@@ -20,7 +17,7 @@ public class HelloApiTest {
         TestRestTemplate rest = new TestRestTemplate();
 
         ResponseEntity<String> res =
-                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class,"Spring");
 
         //status code 200
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -28,5 +25,15 @@ public class HelloApiTest {
         assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         //body Hello Spring
         assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+
+    @Test
+    void failsHelloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
