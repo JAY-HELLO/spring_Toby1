abc0fe8ca5c6d687c70d7cd12438f9967b622b5d
Double_J <double04j@gmail.com>
2023-05-07 22:05:47 +0900

    에노테이션 리팩토링

--- src/main/java/core/config/MySpringBootApplication.java
+++ src/main/java/core/config/MySpringBootApplication.java
@@ -1,7 +1,11 @@
-package core.spring_toby;
+package core.config;
 
+import core.config.EnableMyAutoConfiguration;
+import core.config.autoconfig.DispatcherServletConfig;
+import core.config.autoconfig.TomcatWebServerConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,5 +16,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Configuration
 @ComponentScan//@Component가 붙은 모든 클래스를 빈에 등록
-public @interface MySpringBootAnnotation {
+@EnableMyAutoConfiguration
+public @interface MySpringBootApplication {
 }
