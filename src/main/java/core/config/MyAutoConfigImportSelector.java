6501ff0ab839260441336ffdf2019d78f2dee34c
Double_J <double04j@gmail.com>
2023-05-08 16:24:20 +0900

    import selector 적용

---
+++ src/main/java/core/config/MyAutoConfigImportSelector.java
@@ -0,0 +1,14 @@
+package core.config;
+
+import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class MyAutoConfigImportSelector implements DeferredImportSelector{
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        return new String[]{
+                "core.config.autoconfig.DispatcherServletConfig",
+                "core.config.autoconfig.TomcatWebServerConfig"
+        };
+    }
+}
