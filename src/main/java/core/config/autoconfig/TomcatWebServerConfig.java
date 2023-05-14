1ce7e76889a94674c0bc0d0f85c2dd0b5789f5d2
Double_J <double04j@gmail.com>
2023-05-14 21:10:13 +0900

    tomcat 과 jetty를 conditionalMyOnClass 어노테이션과 MyOnClassCondition을 이용해 사용여부 결정 자동화

--- src/main/java/core/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/core/config/autoconfig/TomcatWebServerConfig.java
@@ -1,23 +1,19 @@
 package core.config.autoconfig;
 
+import core.config.ConditionalMyOnClass;
 import core.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.ToncatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("TomcatWebServerFactory")// 빈 이름 직접 지정
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
 
-    static class ToncatCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
-        }
-    }
 }
