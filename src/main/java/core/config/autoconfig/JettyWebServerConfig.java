e4fd5545dd6c9ac9a36b75b5472bafbb592010d9
Double_J <double04j@gmail.com>
2023-05-14 21:32:35 +0900

    자동 구성정보 와 사용자 구성정보의 동작확인

--- src/main/java/core/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/core/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package core.config.autoconfig;
 
 import core.config.ConditionalMyOnClass;
 import core.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -16,6 +17,7 @@ import org.springframework.util.ClassUtils;
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
--- src/main/java/core/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/core/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package core.config.autoconfig;
 
 import core.config.ConditionalMyOnClass;
 import core.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
@@ -12,6 +13,7 @@ import org.springframework.util.ClassUtils;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("TomcatWebServerFactory")// 빈 이름 직접 지정
+    @ConditionalOnMissingBean//해당타입의 빈이 사용자가 먼저 등록 한 것이 없다면 그때 동록하도록 함.
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
