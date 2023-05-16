abce23ff4eb138925d809b687d221316ee681e3d
Double_J <double04j@gmail.com>
2023-05-16 12:17:49 +0900

    자동구성 우선순위 확인

--- src/main/java/core/spring_toby/SpringTobyApplication.java
+++ src/main/java/core/spring_toby/SpringTobyApplication.java
@@ -1,10 +1,22 @@
 package core.spring_toby;
 
 import core.config.MySpringBootApplication;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class SpringTobyApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env){
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("My name = " + name);
+        };
+
+    }
 
     public static void main(String[] args) {
         // 처음 스프링 부트 어플리케이션을 생성할떄의 모양과 동일함을 알 수 있다.
