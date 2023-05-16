83e9f6158445a32ad767e0dd44ea479414b2ffa9
Double_J <double04j@gmail.com>
2023-05-16 14:00:06 +0900

    자동구성

--- src/main/java/core/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/core/config/autoconfig/TomcatWebServerConfig.java
@@ -1,19 +1,17 @@
 package core.config.autoconfig;
 
 import core.config.ConditionalMyOnClass;
+import core.config.EnableMyConfigurationProperties;
 import core.config.MyAutoConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 @Import(ServerProperties.class)
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatWebServerConfig {
 
     @Bean("TomcatWebServerFactory")// 빈 이름 직접 지정
