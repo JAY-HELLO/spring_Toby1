37e3f0242788b88139b46e3d8d13b5db7e9b8538
Double_J <double04j@gmail.com>
2023-05-16 12:24:43 +0900

    env 를 매번 받아오지 않고 필드에서 받기

--- src/main/java/core/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/core/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package core.config.autoconfig;
 
 import core.config.ConditionalMyOnClass;
 import core.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -13,11 +14,15 @@ import org.springframework.util.ClassUtils;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("TomcatWebServerFactory")// 빈 이름 직접 지정
     @ConditionalOnMissingBean//해당타입의 빈이 사용자가 먼저 등록 한 것이 없다면 그때 동록하도록 함.
     public ServletWebServerFactory servletWebServerFactory(Environment env) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 
