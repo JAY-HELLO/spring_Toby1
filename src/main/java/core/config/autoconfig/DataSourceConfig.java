e56f18a9a30c5314f385f063aa0d433001247879
Double_J <double04j@gmail.com>
2023-05-18 13:44:53 +0900

    datasource config 자동구성

---
+++ src/main/java/core/config/autoconfig/DataSourceConfig.java
@@ -0,0 +1,46 @@
+package core.config.autoconfig;
+
+import com.zaxxer.hikari.HikariDataSource;
+import core.config.ConditionalMyOnClass;
+import core.config.EnableMyConfigurationProperties;
+import core.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
+
+@MyAutoConfiguration
+@ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")//해당 클래스가 존재하면 자동구성이 실행됨.
+@EnableMyConfigurationProperties(MyDataSourceProperties.class)//자동구성 정보가 충족될떄만 빈으로 등록되도록 함
+public class DataSourceConfig {
+
+    @Bean
+    @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
+    DataSource hikariDataSource(MyDataSourceProperties properties) {
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setDriverClassName(properties.getDriverClassName());
+        dataSource.setJdbcUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean//HIKARI가 만들어지지 않았을 경우 밑의 dataSource를 생성.
+    DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(properties.getDriverClassName()));
+        dataSource.setUrl(properties.getUrl());
+        dataSource.setUsername(properties.getUsername());
+        dataSource.setPassword(properties.getPassword());
+
+        return dataSource;
+    }
+
+}
