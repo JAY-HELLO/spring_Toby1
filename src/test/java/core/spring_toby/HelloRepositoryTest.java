534490a694b4443fcd919c9b12de159efaeab95a
Double_J <double04j@gmail.com>
2023-05-18 22:00:01 +0900

    JdbcTemplate로 helloRepository 구성

---
+++ src/test/java/core/spring_toby/HelloRepositoryTest.java
@@ -0,0 +1,41 @@
+package core.spring_toby;
+
+import core.spring_toby.study.HellobootTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@HellobootTest
+@Transactional
+public class HelloRepositoryTest {
+    @Autowired HelloRepository helloRepository;
+    @Autowired JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void init(){
+        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+    }
+
+    @Test
+    void findHelloFailed(){
+
+        assertThat(helloRepository.findHello("toby")).isNull();
+    }
+
+    @Test
+    void increaseCount(){
+        assertThat(helloRepository.countOf("toby")).isEqualTo(0);
+
+        helloRepository.increaseCount("toby");
+        assertThat(helloRepository.countOf("toby")).isEqualTo(1);
+
+        helloRepository.increaseCount("toby");
+        assertThat(helloRepository.countOf("toby")).isEqualTo(2);
+
+    }
+}
